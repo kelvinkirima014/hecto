3c626bef98f30ee9dbcb2480edd0ac556672328f
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-09-04 08:31:07 +0300

    read input as bytes

---
+++ Desktop/hecto/target/debug/deps/hecto-174cdd7b88f56b8e.d
@@ -0,0 +1,5 @@
+/home/kelvin/Desktop/hecto/target/debug/deps/hecto-174cdd7b88f56b8e: src/main.rs
+
+/home/kelvin/Desktop/hecto/target/debug/deps/hecto-174cdd7b88f56b8e.d: src/main.rs
+
+src/main.rs:
---
+++ Desktop/hecto/target/debug/hecto.d
@@ -0,0 +1,1 @@
+/home/kelvin/Desktop/hecto/target/debug/hecto: /home/kelvin/Desktop/hecto/src/main.rs

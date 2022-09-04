c08a30a93e8b4ac19a30e82cd16c45abcf148f9b
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-09-04 05:30:23 +0300

    first commit

---
+++ Desktop/hecto/target/debug/deps/hecto-af5635f67e3c673f.d
@@ -0,0 +1,5 @@
+/home/kelvin/Desktop/hecto/target/debug/deps/hecto-af5635f67e3c673f.rmeta: src/main.rs
+
+/home/kelvin/Desktop/hecto/target/debug/deps/hecto-af5635f67e3c673f.d: src/main.rs
+
+src/main.rs:
---
+++ Desktop/hecto/target/debug/deps/hecto-d557e8d8939e9773.d
@@ -0,0 +1,5 @@
+/home/kelvin/Desktop/hecto/target/debug/deps/hecto-d557e8d8939e9773.rmeta: src/main.rs
+
+/home/kelvin/Desktop/hecto/target/debug/deps/hecto-d557e8d8939e9773.d: src/main.rs
+
+src/main.rs:

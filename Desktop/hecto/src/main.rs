f7350d13f5bd0c0fd7e327f4454df805984398d3
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-09-07 16:21:25 +0300

    prin bytes and chars

--- Desktop/hecto/src/main.rs
+++ Desktop/hecto/src/main.rs
@@ -2,16 +2,32 @@ use termion::raw::IntoRawMode;
 use std::io::{ self,stdin,stdout, Read };
 fn main() {
    
-    println!("Hello, World!");
     let _stdout = stdout().into_raw_mode().unwrap();
 
+  //  for b in stdin().bytes(){
+    //    let c = b.unwrap() as char;
+      //  if c == 'q'{
+        //    break;
+        //}
+
+        //println!("{}", c);
+       
     for b in stdin().bytes(){
-        let c = b.unwrap() as char;
+
+        let b = b.unwrap();
+        let c = b as char;
+         
+        if c.is_control(){
+            println!("{}) \r", b);
+        } else {
+            println!("{} ({:?})\r", b, c);
+        }
+    
         if c == 'q'{
             break;
         }
+        }
 
-        println!("{}", c);
-       
     }
-}
+   
+
